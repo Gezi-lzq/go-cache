29f4702d3b7ea1071edb0de63424fbf8773b6805
gezi-lzq <lzqtxwd@gamil.com>
2022-12-10 19:07:00 +0800

    fix a slip bug

--- lru/lru_test.go
+++ lru/lru_test.go
@@ -1,6 +1,9 @@
 package lru
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 type String string
 
@@ -22,12 +25,12 @@ func TestGet(t *testing.T) {
 
 func TestRemoveoldest(t *testing.T) {
 	k1, k2, k3 := "key1", "key2", "k3"
-	vi, v2, v3 := "value1", "value2", "v3"
+	v1, v2, v3 := "value1", "value2", "v3"
 	cap := len(k1 + k2 + v1 + v2)
 	lru := New(int64(cap), nil)
-	lru.Add(k1, string(v1))
-	lru.Add(k2, string(v2))
-	lru.Add(k3, string(v3))
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3))
 
 	if _, ok := lru.Get("key1"); ok || lru.Len() != 2 {
 		t.Fatalf("Removeoldest key1 failed")
